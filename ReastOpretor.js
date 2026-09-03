c7bf9e930596aea733107159dd134071a8168bbf
satyam kushwaha <sattukush18@gmail.com>
2026-09-03 16:21:14 +0530

    Add files via upload

---
+++ ReastOpretor.js
@@ -0,0 +1,12 @@
+function sum(...numbers) {
+    let total = 0;
+
+    for (let num of numbers) {
+        total += num;
+    }
+
+    return total;
+}
+
+console.log(sum(10, 20, 30));
+console.log(sum(1, 2, 3, 4, 5));
