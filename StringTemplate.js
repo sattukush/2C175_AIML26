f5c5fe778d785528709a2c8051a61001624700cd
satyam kushwaha <sattukush18@gmail.com>
2026-09-03 16:21:44 +0530

    Add files via upload

---
+++ StringTemplate.js
@@ -0,0 +1,8 @@
+let first = "Satyam"
+let last = "Kushwaha"
+console.log(`My Name is ${first}${last}`);
+let fullName = `"My Name is " ${first}${last}`;
+
+function fullname(first,last){
+    return `${first}${last}`
+}
