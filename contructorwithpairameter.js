b60c6a4726ac7b2ec9119ae27a5860dac66ea1e3
satyam kushwaha <sattukush18@gmail.com>
2026-09-16 20:24:38 +0530

    Add files via upload

---
+++ contructorwithpairameter.js
@@ -0,0 +1,10 @@
+
+//Pairameter constroctor
+ class student{
+     constructor(name){
+         this.Name = "Satyam";
+     console.log(this.Name);
+     }
+    
+}
+ let s1 = new student("satyam");
