d10aa4b09ef2daee447da70490a099b3d9d009cd
satyam kushwaha <sattukush18@gmail.com>
2026-09-16 20:24:12 +0530

    Add files via upload

---
+++ constructorwithoutpairameter.js
@@ -0,0 +1,12 @@
+// Normal Costructor
+ class Student{
+     constructor(name){
+        this.name = name
+        console.log("Hello Student ")
+        console.log(this.name)
+    
+    }
+}
+let s1 = new Student("Tushar")
+
+
