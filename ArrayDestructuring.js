a3f9dbf991896bbe78c632352963dd62a915753e
satyam kushwaha <sattukush18@gmail.com>
2026-09-16 20:23:48 +0530

    Add files via upload

---
+++ ArrayDestructuring.js
@@ -0,0 +1,10 @@
+let student = ["Satyam", 175, "CSE", 21, "Delhi"];
+
+// Array Destructuring
+let [name, roll, branch, age, city] = student;
+
+console.log("Name:", name);
+console.log("Roll No:", roll);
+console.log("Branch:", branch);
+console.log("Age:", age);
+console.log("City:", city);
